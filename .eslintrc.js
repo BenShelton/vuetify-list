fd95f03fbaf71e85b88b6868948ffb9af03de603
BenShelton <bensheltonjones@gmail.com>
2019-02-27 10:11:39 +0000

    cleanup some initial files

--- .eslintrc.js
+++ .eslintrc.js
@@ -19,6 +19,37 @@ module.exports = {
     'vue/key-spacing': 'error',
     'vue/match-component-file-name': 'error',
     'vue/no-restricted-syntax': 'error',
+    // restrict to 3 attributes per line
+    'vue/max-attributes-per-line': ['error', {
+      'singleline': 3,
+      'multiline': {
+        'max': 1,
+        'allowFirstLine': false
+      }
+    }],
+    // component order
+    'vue/order-in-components': ['error', {
+      'order': [
+        'el',
+        'name',
+        'parent',
+        'functional',
+        ['delimiters', 'comments'],
+        'extends',
+        ['components', 'directives', 'filters'],
+        'mixins',
+        'inheritAttrs',
+        'model',
+        ['props', 'propsData'],
+        'LIFECYCLE_HOOKS',
+        'data',
+        'computed',
+        'watch',
+        'methods',
+        ['template', 'render'],
+        'renderError'
+      ]
+    }],
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
   },
