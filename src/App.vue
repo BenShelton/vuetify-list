6ff2fc41d417a5cf4ab780f402544398aec0ed6f
BenShelton <bensheltonjones@gmail.com>
2019-02-27 11:35:34 +0000

    add link back to home

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,17 @@
         <span>Vuetify</span>
         <span class="font-weight-light ml-3">List Example</span>
       </v-toolbar-title>
+      <v-spacer />
+      <v-toolbar-items v-if="showItems">
+        <v-btn
+          flat
+          exact
+          :to="homeRoute"
+        >
+          <v-icon>keyboard_arrow_left</v-icon>
+          Back to Posts
+        </v-btn>
+      </v-toolbar-items>
     </v-toolbar>
 
     <v-content>
@@ -14,7 +25,21 @@
 </template>
 
 <script>
+import routeNames from '@/router/routeNames'
+
 export default {
-  name: 'App'
+  name: 'App',
+
+  data () {
+    return {
+      homeRoute: { name: routeNames.HOME }
+    }
+  },
+
+  computed: {
+    showItems () {
+      return this.$route.name === routeNames.POST
+    }
+  }
 }
 </script>
